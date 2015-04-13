6d3793f7f205d285de24d711bb3887ff8194db10
Cody Gildea <cbgildea@gmail.com>
2015-04-13 02:36:33 -0700

    Test Ignore

--- GrayCodeHW1/src/com/company/GrayCodeCECS428.java
+++ GrayCodeHW1/src/com/company/GrayCodeCECS428.java
@@ -4,14 +4,30 @@ public class GrayCodeCECS428 {
 
     public static void main(String[] args) {
        // int N = Integer.parseInt(args[0]);
-        int N = 22;
-        gray("", N);
+        int N = 17;
+        BufferedWriter writer = null;
+        try {
+            File output = new File("17N.txt");
+            writer = new BufferedWriter(new FileWriter(output, true));
+            System.out.println(output.getCanonicalPath());
+            gray("", N);
+        } catch (Exception e){
+            e.printStackTrace();
+        } finaly {
+            try{
+                writer.close();
+            } catch (Exception e){
+
+            }
+        }
     }
 
     // append reverse of order n gray code to prefix string, and print
     public static void grayRev(String prefix, int n) {
-        if (n == 0) System.out.println(prefix);
-        else {
+        if (n == 0) { //System.out.println(prefix);
+            writer.write(java.text.MessageFormat.format("{0}\n", prefix))
+        }
+            else {
             gray(prefix + "1", n - 1);
             grayRev(prefix + "0", n - 1);
         }
@@ -19,7 +35,9 @@ public class GrayCodeCECS428 {
 
     // append order n gray code to end of prefix string, and print
     public static void gray(String prefix, int n) {
-        if (n == 0) System.out.println(prefix);
+        if (n == 0){ //System.out.println(prefix);
+            writer.write(java.text.MessageFormat.format("{0}\n", prefix))
+        }
         else {
             gray(prefix + "0", n - 1);
             grayRev(prefix + "1", n - 1);
